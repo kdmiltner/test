c3d793533a0ff1f96df2a89e77d2583636265d7e
KayDee Miltner <kaydeemiltner@gmail.com>
2023-05-17 12:09:04 -0400

    Implementing for loop that prints 0 - 9

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
 # Adding test file
+
+def run_test():
+    """Kicks off a for loop that prints output
+    """
+    i = 0
+    for i in range(10):
+        print(i)
+        i+=1
+
+
+if __name__ == '__main__':
+    run_test()
